8157801e90bac60c453cb88b13c7025d768d4a79
Guilherme Fujiyoshi <gfuji1211@gmail.com>
2020-09-08 17:43:16 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import 'reflect-metadata';
-
-import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import routes from './routes';
+import 'reflect-metadata';
+import uploadConfig from './config/upload';
 import './database';
 import AppError from './errors/AppError';
-import uploadConfig from './config/upload';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
